430905691460f771b7af593f203a897b62613609
HleyHub <hashleyed@gmail.com>
2023-04-25 12:17:01 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * times_table - A function that adds two integers and returns the result
+ * add - A function that adds two integers and returns the result
  * @i1: The first interger to be returned
  * @i2: The second interger to be returned
  * Return: integer value
