0b5f8082e3b4f5495207150f42bc03d536476e89
HleyHub <hashleyed@gmail.com>
2023-04-27 12:55:05 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,22 +10,27 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 15) == 0)
+		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("FizzBuzz\t");
+			printf(" Fizz");
 		}
-		else if ((i % 3) == 0)
+		else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf("Fizz\t");
+			printf(" Buzz");
 		}
-		else if ((i % 5) == 0)
+		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("Buzz\t");
+			printf(" FizzBuzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
 		}
 		else
 		{
-			printf("%d\t", i);
+			printf(" %d", i);
 		}
 	}
+	printf("\n");
 	return (0);
 }
