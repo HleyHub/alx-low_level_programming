7124e5c3a68208a30eddc86e9e23ad2ca733fc07
HleyHub <hashleyed@gmail.com>
2023-05-04 11:05:31 -0700

    5-string_toupper.c Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * string_toupper - Function that changes all lowercase
+ * letters of a string to uppercase
+ * @strg: String
+ */
+char *string_toupper(char *strg)
+{
+	int j;
+
+	for (j = 0; strg[j] != '\0'; j++)
+	{
+		if (strg[j] >= 'a' && strg[j] <= 'z')
+		{
+			strg[j] = strg[j] - 'a' + 'A';
+		}
+	}
+	return (strg);
+}
