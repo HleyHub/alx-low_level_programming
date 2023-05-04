5f2c27cce51adda3c9ed3d90a8e26e8260ead425
HleyHub <hashleyed@gmail.com>
2023-05-04 07:40:07 -0700

    2-strncpy.c Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncpy - Function that concatenates two strings
+ * @dest: First string
+ * @src: second string
+ * @n: number of bytes
+ * Return: 0
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	if (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
