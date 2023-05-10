32b3fef3437436d67de5ef8972048934cacdd3fe
HleyHub <hashleyed@gmail.com>
2023-05-10 05:01:40 -0700

    101-wildcmp.c Task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,21 +8,23 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
+	int i = 0;
+
+	if (s1[i] == '\0' && s2[i] == '\0')
 	{
 		return (1);
 	}
-	if (*s1 == *s2)
+	if (s1[i] == s2[i])
 	{
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
-	if (*s2 == '*')
+	if (s2[i] == '*')
 	{
 		if (wildcmp(s1, s2 + 1))
 		{
 			return (1);
 		}
-		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
+		if (s1[i] != '\0' && wildcmp(s1 + 1, s2))
 		{
 			return (1);
 		}
