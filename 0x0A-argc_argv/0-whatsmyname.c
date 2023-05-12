7460bd15dc4433aa9e1cdb5b8f1eb77937acc385
HleyHub <hashleyed@gmail.com>
2023-05-12 06:09:17 -0700

    0-whatsmyname.c Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Print the name of the program
+ * @argc: Count the number of arguments
+ * @argv: Array of string
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
