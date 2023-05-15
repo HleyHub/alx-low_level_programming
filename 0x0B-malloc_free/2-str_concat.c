b33171c84dba7497e9b0ae266ed68b661b1b8a4f
HleyHub <hashleyed@gmail.com>
2023-05-15 03:41:32 -0700

    2-str_concat.c Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,27 +9,33 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned ind, strlgth1, strlgth2, size;
+	unsigned int ind1, ind2, strlgth1, strlgth2, size;
+	char *s3;
 
 	strlgth1 = strlen(s1);
 	strlgth2 = strlen(s2);
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
 		return ("");
 	}
-	size = strlgth1 + strlgth2 +1;
-	s1 = (char *)malloc(size * sizeof(char));
-	if (s1 == NULL)
+	if (s2 == NULL)
+	{
+		return ("");
+	}
+	size = strlgth1 + strlgth2 + 1;
+	s3 = (char *)malloc(size * sizeof(char));
+	if (s3 == NULL)
 	{
 		return (NULL);
 	}
-	for (ind = 0; ind <= strlgth2; ind++)
+	for (ind1 = 0; ind1 < strlgth1; ind1++)
 	{
-		s1[strlgth1 + ind] = s2[ind];
+		s3[ind1] = s1[ind1];
 	}
-	return (s1);
-/*	if (s1[strlgth1 + strlgth2] != '\0')
+	for (ind2 = 0; ind2 < strlgth2; ind2++)
 	{
-		return (s1);
-	}*/
+		s3[ind1 + ind2] = s2[ind2];
+	}
+	s3[ind1 + ind2] = '\0';
+	return (s3);
 }
