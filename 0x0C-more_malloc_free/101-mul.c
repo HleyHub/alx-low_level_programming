b5042faae97ac2fd7f911936921774c018b0e88d
HleyHub <hashleyed@gmail.com>
2023-05-18 11:38:31 -0700

    101-mul.c a program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,26 +9,27 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, mul;
+	int num1, num2, mul, ind1, ind2;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	for (ind1 = 1; ind1 < argc; ind1++)
+	{
+		for (ind2 = 0; argv[ind1][ind2] != '\0'; ind2++)
+		{
+			if (!isdigit(argv[ind1][ind2]))
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
-	if (!isdigit(num1) && !isdigit(num2))
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	else
-	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		mul = num1 * num2;
-	}
+	mul = num1 * num2;
 	printf("%d\n", mul);
-	return (0);
+	return 0;
 }
