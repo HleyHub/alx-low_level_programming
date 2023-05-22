5064ec496286f3d01a84be94238c562fb0becb26
HleyHub <hashleyed@gmail.com>
2023-05-22 04:09:55 -0700

    2-print_dog.c a function that prints a struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * print_dog - function that prints a struct dog
  * @d: Type
@@ -8,7 +9,7 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 	{
-		return;
+		exit(98);
 	}
 	if ((*d).name == NULL)
 	{
