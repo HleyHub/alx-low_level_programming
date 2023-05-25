fa3c75cc3218e2192f175264e337dfe8d919456a
HleyHub <hashleyed@gmail.com>
2023-05-25 02:38:35 -0700

    A function that prints numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,11 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		num = va_arg(list, int);
 		printf("%d", num);
-		if (separator == NULL)
-		{
-			continue;
-		}
-		if (ind < n - 1)
+		if (separator != NULL && ind < n - 1)
 		{
 			printf("%s", separator);
 		}
