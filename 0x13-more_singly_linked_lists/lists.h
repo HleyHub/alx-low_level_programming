d932266eab3ab39c4b3a12e240d4c5d79a25e05c
HleyHub <hashleyed@gmail.com>
2023-06-06 11:52:16 -0700

    lists.h updated with betty

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stddef.h>
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -12,8 +11,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 int _putchar(char c);
