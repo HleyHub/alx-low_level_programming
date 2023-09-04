618d157e34d17c7fbcebf1c299b4508d79927336
HleyHub <hashleyed@gmail.com>
2023-09-04 06:45:44 -0700

    Updated main.h file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,12 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define BUFFER_SIZE 1024
+#define READ_BUF_SIZE 1024
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
